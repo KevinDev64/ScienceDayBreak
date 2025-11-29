97dbb406e11ae196e691b37bec07414c7849c904
KevinDev64 <kevindev56@yandex.ru>
2025-11-29 03:29:29 +0300

    feat: skip when not main branch

--- updater.sh
+++ updater.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
 import os
+import subprocess
 
 def execute_com(command):
-	code = os.system(command)
-	if code != 0:
-		print(f"Failed with code {code} when running the command `{command}`")
-		exit(1)
+	if command == "git pull":
+		result = subprocess.check_output(command, shell=True)
+		if result == b'Already up to date.\n':
+			print("Updater! -> NO UPDATES!")
+			exit(0)
+	else:
+		code = os.system(command)
+		if code != 0:
+			print(f"Failed with code {code} when running the command `{command}`")
+			exit(1)
 
 result_codes = []
 commands = [
@@ -16,6 +23,7 @@ commands = [
 	"docker compose down",
 	"docker compose up -d"
 ]
+
 
 for command in commands:
 	execute_com(command)
